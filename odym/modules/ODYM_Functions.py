047afcd06e116ac50d20f70dab0d9902b8241328
Chris Hauenstein <104057845+chauenstein@users.noreply.github.com>
2023-08-15 12:29:05 +0200

    Adapt check_dataset to check also in data subfolders

--- odym/modules/ODYM_Functions.py
+++ odym/modules/ODYM_Functions.py
@@ -1413,19 +1413,21 @@ def convert_log(file, file_format='html'):
     output = pypandoc.convert_file(file, file_format, outputfile=output_filename)
     assert output == ""
 
-def check_dataset(path,PL_Names,PL_Version,Mylog):
+def check_dataset(path,PL_Names,PL_Version,PL_SubFolder,Mylog):
     """
-    Checks that every parameter in Pl_Names with the corrsponding version PL_Versions is in the folder given by path
+    Checks that every parameter in Pl_Names with the corrsponding version PL_Versions is in the folder given by path, or subfolder given by PL_SubFolder
 
     :param path: Dataset folder
     :param PL_Names: List of parameters names
     :param PL_versions: List of parameters versions
+    :param PL_SubFolder: List of data subfolder names
     :param Mylog: log file
 
     """
     for m in range(len(PL_Names)):
         if PL_Names[m]+'_'+PL_Version[m]+'.xlsx' not in os.listdir(path):
-            Mylog.error(PL_Names[m]+'_'+PL_Version[m]+'.xlsx not in the dataset.')
+            if PL_Names[m]+'_'+PL_Version[m]+'.xlsx' not in os.listdir(os.path.join(path, PL_SubFolder[m])):
+                Mylog.error(PL_Names[m]+'_'+PL_Version[m]+'.xlsx not in the dataset.')
     
     
 # The End
